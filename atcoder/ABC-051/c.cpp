73469bb8f1bedce66abfd2d5376abd984cac7112
YusukeKato <s1526035cn@s.chibakoudai.jp>
2019-02-16 19:16:40 +0900

    ABC-051 C

---
+++ atcoder/ABC-051/c.cpp
@@ -0,0 +1,48 @@
+// 解説読んだ
+// 条件を見逃した
+// sx < tx, sy < ty の条件がなくなったらかなり面倒くさい気がする
+
+/**
+ * 条件、sx < tx, sy < ty が重要
+ * この条件があるから考えるケースはかなり限られる
+ * つまり、直線（真上とか）がない、マイナス方向もない
+ */
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int sx, sy, tx, ty;
+    cin >> sx >> sy >> tx >> ty;
+
+    // 距離の差分をとる
+    int dx = tx - sx;
+    int dy = ty - sy;
+
+    // 出力する文字列
+    string s = "";
+
+    // 1
+    for(int i = 0; i < dx; i++) s += 'R';
+    for(int i = 0; i < dy; i++) s += 'U';
+
+    // 2
+    for(int i = 0; i < dx; i++) s += 'L';
+    for(int i = 0; i < dy; i++) s += 'D';
+
+    // 3
+    s += 'D';
+    for(int i = 0; i < dx+1; i++) s += 'R';
+    for(int i = 0; i < dy+1; i++) s += 'U';
+    s += 'L';
+
+    // 4
+    s += 'U';
+    for(int i = 0; i < dx+1; i++) s += 'L';
+    for(int i = 0; i < dy+1; i++) s += 'D';
+    s += 'R';
+
+    cout << s;
+}
