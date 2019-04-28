dd7e52d4097984d1c7659237d2402df02c2c9a68
YusukeKato <s1526035cn@s.chibakoudai.jp>
2019-04-28 09:55:16 +0900

    Update c.cpp

--- atcoder/ABC-125/c.cpp
+++ atcoder/ABC-125/c.cpp
@@ -19,71 +19,35 @@ int main()
 	vector<long long> v(N, 0);
 	for(int i = 0; i < N; i++) cin >> v[i];
 
-	long long minval = 1e10;
-	long long maxval = 0;
-	int idx = -1;
-	int idx2 = -1;
-	for(int i = 0; i < N-1; i++)
-	{
-		int a = gcd(v[i], v[i+1]);
-		if(a < minval)
-		{
-			idx = i;
-			minval = a;
-		}
-		if(a > maxval)
-		{
-			idx2 = i;
-			maxval = a;
-		}
-	}
+	vector<long long> left(N, 0);
+	vector<long long> right(N, 0);
 
-	long long subv1 = v[idx];
-	long long subv2 = v[idx+1];
-
-	long long subv21 = v[idx2];
-	long long subv22 = v[idx2+1];
-
-	vector<long long> tt(4);
-
-	v[idx] = subv21;
-	v[idx+1] = subv2;
-	tt[0] = v[0];
+	long long tmp = v[0];
+	left[0] = v[0];
 	for(int i = 1; i < N; i++)
 	{
-		tt[0] = gcd(tt[0], v[i]);
+		tmp = gcd(tmp, v[i]);
+		left[i] = tmp;
 	}
-
-	v[idx] = subv1;
-	v[idx+1] = subv21; 
-	tt[1] = v[0];
-	for(int i = 1; i < N; i++)
+	tmp = v[N-1];
+	right[N-1] = v[N-1];
+	for(int i = N-2; i >= 0; i--)
 	{
-		tt[1] = gcd(tt[1], v[i]);
+		tmp = gcd(tmp, v[i]);
+		right[i] = tmp;
 	}
 
-	v[idx] = subv22;
-	v[idx+1] = subv2;
-	tt[2] = v[0];
-	for(int i = 1; i < N; i++)
-	{
-		tt[2] = gcd(tt[2], v[i]);
-	}
-
-	v[idx] = subv1;
-	v[idx+1] = subv22; 
-	tt[3] = v[0];
-	for(int i = 1; i < N; i++)
-	{
-		tt[3] = gcd(tt[3], v[i]);
-	}
-
-	//output
 	long long ans = 0;
-	for(int i = 0; i < 4; i++)
+	for(int i = 0; i < N; i++)
 	{
-		if(tt[i] > ans) ans = tt[i];
+		long long sub;
+		if(i == 0) sub = right[i+1]; 
+		else if(i == N-1) sub = left[i-1];
+		else sub = gcd(left[i-1], right[i+1]);
+
+		if(ans < sub) ans = sub;
 	}
+
 	cout << ans << endl;
 
 	return 0;
